7a3f8c7b608c36ea21f079b4a265033af3740b35
Fernando Guillen <fguillen.mail@gmail.com>
2023-08-03 18:51:26 +0200

    Reorganizing

--- tool_box/autoloads/groups_utils.gd
+++ tool_box/autoloads/groups_utils.gd
@@ -10,11 +10,6 @@ extends Node
 # 07. constants
 # 08. exported variables
 # 09. public variables
-var frogy: Frogy : get = _get_frogy
-var coco: Coco : get = _get_coco
-var entities_layer: Node2D : get = _get_entities_layer
-var foreground_layer: Node2D : get = _get_foreground_layer
-
 # 10. private variables
 var _cache = {}
 
@@ -25,23 +20,7 @@ var _cache = {}
 # 14. built-in virtual _ready method
 # 15. remaining built-in virtual methods
 # 16. public methods
-# 17. private methods
-func _get_frogy() -> Frogy:
-	return _get_first_node_in_group("frogy") as Frogy
-	
-	
-func _get_coco() -> Coco:
-	return _get_first_node_in_group("coco") as Coco
-	
-	
-func _get_entities_layer() -> Node2D:
-	return _get_first_node_in_group("entities_layer") as Node2D
-	
-	
-func _get_foreground_layer() -> Node2D:
-	return _get_first_node_in_group("foreground_layer") as Node2D
-	
-	
+# 17. private methods		
 func _get_first_node_in_group(group_name: String) -> Node:
 	if _cache.has(group_name) and is_instance_valid(_cache[group_name]):
 		return _cache[group_name]
